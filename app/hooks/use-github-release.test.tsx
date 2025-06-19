670612d35480a38eea7bd5736e3d8f69ed929fea
Rudy Orre <rudyorre@gmail.com>
2025-06-18 20:34:17 -0700

    Fix test import

--- app/hooks/use-github-release.test.tsx
+++ app/hooks/use-github-release.test.tsx
@@ -1,5 +1,5 @@
 import { renderHook, waitFor } from '@testing-library/react';
-import { useGitHubRelease } from '../use-github-release';
+import { useGitHubRelease } from './use-github-release';
 
 // Mock localStorage
 const localStorageMock = {
